c38bf4ae02dcfafdbd693df318be5650da3b1bbb
hong0220 <1023746826@qq.com>
2016-06-17 21:27:30 +0800

    宿舍，测试代码

--- src/main/java/cms/Appliaction.java
+++ src/main/java/cms/Appliaction.java
@@ -2,8 +2,10 @@ package cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:db-config.properties"})
 public class Appliaction {
 
     public static void main(String[] args) {
--- src/test/java/ShiroDbRealmTest.java
+++ src/test/java/ShiroDbRealmTest.java
@@ -10,27 +10,23 @@ public class ShiroDbRealmTest {
         // 加密迭代次数
         int iterations = 1024;
 
-        // true=Hex存储(占数据库64位字符)，false=Base64存储(占数据库44位字符)
-        boolean isStoredHex = false;
+        // Hex存储(占数据库64位字符)，Base64存储(占数据库44位字符)
+        String result = new Md2Hash("123", null, iterations).toHex();
+        result = new Md5Hash("123", null, iterations).toHex();
+        result = new Sha1Hash("123", null, iterations).toHex();
+        result = new Sha256Hash("123", null, iterations).toHex();
+        result = new Sha384Hash("123", null, iterations).toHex();
+        result = new Sha512Hash("123", null, iterations).toHex();
+        result = new Sha256Hash("123", null, iterations).toHex();
 
-        String result = "";
-        if (isStoredHex) {
-            result = new Md2Hash("123", null, iterations).toHex();
-            result = new Md5Hash("123", null, iterations).toHex();
-            result = new Sha1Hash("123", null, iterations).toHex();
-            result = new Sha256Hash("123", null, iterations).toHex();
-            result = new Sha384Hash("123", null, iterations).toHex();
-            result = new Sha512Hash("123", null, iterations).toHex();
-            result = new Sha256Hash("123", null, iterations).toHex();
-        } else {
-            result = new Md2Hash("123", null, iterations).toBase64();
-            result = new Md5Hash("123", null, iterations).toBase64();
-            result = new Sha1Hash("123", null, iterations).toBase64();
-            result = new Sha256Hash("123", null, iterations).toBase64();
-            result = new Sha384Hash("123", null, iterations).toBase64();
-            result = new Sha512Hash("123", null, iterations).toBase64();
-            result = new Sha256Hash("123", null, iterations).toBase64();
-        }
+        result = new Md2Hash("123", null, iterations).toBase64();
+        result = new Md5Hash("123", null, iterations).toBase64();
+        result = new Sha1Hash("123", null, iterations).toBase64();
+        result = new Sha256Hash("123", null, iterations).toBase64();
+        result = new Sha384Hash("123", null, iterations).toBase64();
+        result = new Sha512Hash("123", null, iterations).toBase64();
+        result = new Sha256Hash("123", null, iterations).toBase64();
+
         System.out.println("输出结果：" + result);
     }
 }
