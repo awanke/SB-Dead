298f4f612bfa5099de6b9765c359f0fb5c817a28
hong0220 <1023746826@qq.com>
2016-06-18 10:16:04 +0800

    静态化页面继续

---
+++ src/main/java/cms/config/GlobalConfig.java
@@ -0,0 +1,18 @@
+package cms.config;
+
+import cms.utils.ConfigUtil;
+
+public class GlobalConfig {
+    public static String websiteUr = ConfigUtil.getValue("website.url");
+
+    public static String solrUrlArticle = ConfigUtil.getValue("solr.url.article");
+
+    public static String solrUrlSearchlog = ConfigUtil.getValue("solr.url.searchlog");
+
+    public static String staticPageSize = ConfigUtil.getValue("static.page.size");
+
+
+
+
+    public static boolean isHex = ConfigUtil.getBooleanValue("password.is.stored.hex");
+}
