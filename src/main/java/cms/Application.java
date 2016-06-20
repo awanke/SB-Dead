85ee507d16a5d39d238c63e07b01f8d15faba87e
hongyh <hongyh@qq.com>
2016-06-20 15:34:20 +0800

    公司部署项目，注释掉暂时不用的东西

--- src/main/java/cms/Application.java
+++ src/main/java/cms/Application.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource({"classpath:db-config.properties"})
-public class Appliaction {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Appliaction.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
