1882be5f4f12a8dd9c22af33acf58a2890a9131a
hongyh <1023746826@qq.com>
2016-06-26 13:34:21 +0800

    处理文件上传大小问题

--- src/main/java/cms/Application.java
+++ src/main/java/cms/Application.java
@@ -1,14 +1,34 @@
 package cms;
 
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
+//exclude表示自动配置时不包括Multipart配置
+@EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
 @PropertySource({"classpath:db-config.properties"})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    //显示声明CommonsMultipartResolver为mutipartResolver
+    @Bean(name = "multipartResolver")
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("UTF-8");
+        //resolveLazily属性启用是为了推迟文件解析,以在UploadAction中捕获文件大小异常
+        resolver.setResolveLazily(true);
+        resolver.setMaxInMemorySize(40960);
+        resolver.setMaxUploadSize(50 * 1024 * 1024);//上传文件大小 50M 50*1024*1024
+        return resolver;
     }
 }
