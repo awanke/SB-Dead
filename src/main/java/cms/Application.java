f52a1c9e4a36cf55052ba7a6f5649363dd2258ed
hong0220 <1023746826@qq.com>
2016-07-15 08:11:52 +0800

    真难写这条sql

--- src/main/java/cms/Application.java
+++ src/main/java/cms/Application.java
@@ -1,6 +1,5 @@
 package cms;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +10,7 @@ import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
-//exclude表示自动配置时不包括Multipart配置
+// exclude表示自动配置时不包括Multipart配置
 @EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
 @PropertySource({"classpath:db-config.properties"})
 public class Application {
@@ -20,15 +19,16 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    //显示声明CommonsMultipartResolver为mutipartResolver
+    // 显示声明CommonsMultipartResolver为mutipartResolver
     @Bean(name = "multipartResolver")
     public MultipartResolver multipartResolver() {
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
         resolver.setDefaultEncoding("UTF-8");
-        //resolveLazily属性启用是为了推迟文件解析,以在UploadAction中捕获文件大小异常
+        // resolveLazily属性启用是为了推迟文件解析,以在UploadAction中捕获文件大小异常
         resolver.setResolveLazily(true);
         resolver.setMaxInMemorySize(40960);
-        resolver.setMaxUploadSize(50 * 1024 * 1024);//上传文件大小 50M 50*1024*1024
+        // 上传文件大小 50M 50*1024*1024
+        resolver.setMaxUploadSize(50 * 1024 * 1024);
         return resolver;
     }
 }
